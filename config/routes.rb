0d9796a64f0e190eed0383f1791b2957e0e2f80c
go2023 <tgoncharenko2013@gmail.com>
2023-11-28 22:42:39 -0800

    devise/registration_controller and devise/session_controller were customized

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'current_user', to: 'current_user#index'
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+   controllers: {
+     sessions: 'users/sessions',
+     registrations: 'users/registrations'
+   }
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
